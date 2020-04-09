71c57b60d86f48c8184c32fcc1fc029633f09aa5
Kirbyasdf <kirbyAugustus@gmail.com>
2020-04-08 23:09:16 -0400

    rails running

--- aws-setup.sh
+++ aws-setup.sh
@@ -22,11 +22,13 @@
 
     # On the Inbound tab, create the following rules (choose Add Rule for each new rule), and then choose Create:
 
-    # Choose HTTP from the Type list, and make sure that Source is set to Anywhere (0.0.0.0/0).
+    #  HTTP from the Type list, and make sure that Source is set to Anywhere (0.0.0.0/0).
 
-    # Choose HTTPS from the Type list, and make sure that Source is set to Anywhere (0.0.0.0/0).
+    #  HTTPS from the Type list, and make sure that Source is set to Anywhere (0.0.0.0/0)
 
-    # Choose SSH from the Type list. In the Source box, choose My IP to automatically populate the field with the public IPv4 address of your local computer. Alternatively, choose Custom and specify the public IPv4 address of your computer or network in CIDR notation. To specify an individual IP address in CIDR notation, add the routing suffix /32, for example, 203.0.113.25/32. If your company allocates addresses from a range, specify the entire range, such as 203.0.113.0/24.
+    # CUSTOM TCP -port -3000 (or whatever port you plan to run ur server on) +  (0.0.0.0/0)
+
+    #  SSH from the Type list. In the Source box, choose My IP to automatically populate the field with the public IPv4 address of your local computer. Alternatively, choose Custom and specify the public IPv4 address of your computer or network in CIDR notation. To specify an individual IP address in CIDR notation, add the routing suffix /32, for example, 203.0.113.25/32. If your company allocates addresses from a range, specify the entire range, such as 203.0.113.0/24.
 
     # Open the Amazon EC2 console at https://console.aws.amazon.com/ec2/.
 
@@ -52,19 +54,75 @@
 
 #view your instance on the ec2 dashboard, copy the DNS
 
-#cd into the folder where you have ur .pem key downloaded
+#u can run the following command with path to ur ssh or cd into the folder where you have ur .pem key downloaded
 
 # ssh into the server using the following in terminal
-#   ssh -i YOUR-KEY.pem ec2-user@YOUR-INSTANCE-DNS-URL
+#   ssh -i YOUR-KEY.pem(or path) ec2-user@YOUR-INSTANCE-DNS-URL
 
-# PHASE 1 COMPLETE
-
-# PHASE 2 
 
 
 # update the server 
     # sudo yum update
 
-#install ruby 
-    # sudo yum install ruby
 
+
+# PHASE 1 COMPLETE
+
+# PHASE 2 
+
+# there are two ways we can go about this, installing via git or wget, for my sake I am going to do niether and just run a rails basic homepage to prove that the server is up and running
+
+#if u would rather build your rails app locally then wget/git clone it into it, run the following commands
+
+
+    #GIT
+        #sudo yum install git-all
+
+    #wget
+
+
+
+# install dependencies 
+    #sudo yum install -y curl gpg gcc gcc-c++ make
+
+
+# install RMV (ruby version manager)
+
+    #keys
+
+        #gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+    #we're going to install rails and ruby w/ RVM in one line
+
+        #curl -sSL https://get.rvm.io | bash -s stable --rails
+
+    #now we're going to run a logic script to let us call rmv
+
+        #if sudo grep -q secure_path /etc/sudoers; then sudo sh -c "echo export rvmsudo_secure_path=1 >> /etc/profile.d/rvm_secure_path.sh" && echo Environment variable installed; fi
+
+    #lets check out we have a clean install with
+        #rvm requirements run
+
+# now we need to install node
+
+    #curl --silent --location https://rpm.nodesource.com/setup_12.x | sudo bash -
+
+    #sudo yum install -y nodejs
+
+# now install yarn
+
+    #curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+    
+    #sudo dnf install yarn
+
+
+
+# if you want git/wget use git clone/weget ur repo otherwise
+
+    #rails new <YOURP-PROJECT> --skip-git
+
+# now run your server w/ the command rails s + -b 0.0.0.0 (aws makes u add the -b 0 etc)
+
+    #rails s -b 0.0.0.0 
+    #for custom port 
+    #rails s -p <urPOrt> -b 0.0.0.0
